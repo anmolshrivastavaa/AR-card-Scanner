ee037fb094aaa317f64a74751e4b2a4130caf8c5
Anmol Shrivastava <147837521+anmolshrivastavaa@users.noreply.github.com>
2025-09-11 15:09:36 +0530

    Create LinkManager.cs

---
+++ LinkManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinkManager : MonoBehaviour
+{
+    public void OpenLinkedin(){
+        Application.OpenURL("https://www.linkedin.com/in/anmolshrivastava17/");
+    }
+    public void Codilo(){
+        Application.OpenURL("https://codolio.com/profile/AnmolShrivastava");
+    }
+    public void FIYA(){
+        Application.OpenURL("https://fiya-project.onrender.com/");
+    }
+    public void COC(){
+        Application.OpenURL("https://bennettu-my.sharepoint.com/personal/e23cseu0068_bennett_edu_in/_layouts/15/stream.aspx?id=%2Fpersonal%2Fe23cseu0068%5Fbennett%5Fedu%5Fin%2FDocuments%2FGaming%20Projects%2FClash%20of%20Carnages%20COC%2FGameplay%20Video%2Emkv&nav=eyJyZWZlcnJhbEluZm8iOnsicmVmZXJyYWxBcHAiOiJPbmVEcml2ZUZvckJ1c2luZXNzIiwicmVmZXJyYWxBcHBQbGF0Zm9ybSI6IldlYiIsInJlZmVycmFsTW9kZSI6InZpZXciLCJyZWZlcnJhbFZpZXciOiJNeUZpbGVzTGlua0NvcHkifX0&nav=eyJyZWZlcnJhbEluZm8iOnsicmVmZXJyYWxBcHAiOiJPbmVEcml2ZUZvckJ1c2luZXNzIiwicmVmZXJyYWxBcHBQbGF0Zm9ybSI6IldlYiIsInJlZmVycmFsTW9kZSI6InZpZXciLCJyZWZlcnJhbFZpZXciOiJNeUZpbGVzTGlua0NvcHkifX0&ga=1");
+    }
+    public void Portfolio(){
+        Application.OpenURL("https://anmol-portfolio-bu.vercel.app/");
+    
+}
+}
